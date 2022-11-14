0266dc073b89d99ce36ab00fc8a02ab94c2ffa4e
Adrian Epifanio Rodriguez Hernandez <adrianepi@gmail.com>
2022-11-14 09:44:46 +0000

    Created searcher class (search all files in a directory and subdirectory && renamed reader to file

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@
 #   @Email:              adrianepi@gmail.com
 #   @GitHub:             https://github.com/AdrianEpi
 #   @Last Modified by:   Adrian Epifanio
-#   @Last Modified time: 2022-11-13 18:59:53
+#   @Last Modified time: 2022-11-14 09:43:46
 #   @Description:        ...
 
 
@@ -15,21 +15,35 @@
 import ast
 import inspect
 from modules.ast_module.node import Node
-from modules.reader_module.reader import Reader
+from modules.file_module.file import File
+from modules.file_module.searcher import Searcher
 
 
 filename = "../samples/Simple_Samples/AccessModifiers/Private/main.py"
-reader = Reader(filename)
+file = File(filename)
 
 
-myAst = ast.dump(ast.parse(reader.getData()), annotate_fields=True, include_attributes=False, indent=4)
+myAst = ast.dump(ast.parse(file.getData()), annotate_fields=True, include_attributes=False, indent=4)
 l = myAst.split("\n")
 
-for i in l:
-	tmp = Node(i)
-	#print(tmp.getIndentation())
-	print(tmp.getData())
-	#print(tmp.getRawData())
-	#print("\n")
+path = "./"
+ext = ".py"
+s = Searcher()
+l = s.lookForFiles(path, ext)
+#print(l)
 
-#print(myAst)
+for i in l:
+	f = File(i)
+	classAST = ast.dump(ast.parse(f.getData()), annotate_fields=True, include_attributes=False, indent=4)
+	print("\n\n\n")
+	print(classAST)
+	
+# for i in l:
+# 	tmp = Node(i)
+# 	#print(tmp.getIndentation())
+# 	print(tmp.getData())
+# 	#print(tmp.getRawData())
+# 	#print("\n")
+
+# #print(myAst)
+
--- app/modules/file_module/file.py
+++ app/modules/file_module/file.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 #   @Proyect:            UMLConverter
 #   @Author:             Adrian Epifanio
-#   @File:               reader.py
+#   @File:               file.py
 #   @Author:             Adrian Epifanio
 #   @Date:               2022-11-13 16:24:52
 #   @Email:              adrianepi@gmail.com
 #   @GitHub:             https://github.com/AdrianEpi
 #   @Last Modified by:   Adrian Epifanio
-#   @Last Modified time: 2022-11-13 18:59:34
-#   @Description:        This file describes a reader and its functionality
+#   @Last Modified time: 2022-11-14 09:37:52
+#   @Description:        This file describes a file and its functionality
 
 
-class Reader:
+class File:
 	"""
-	This class describes a reader.
+	This class describes a file.
 	"""
 
 	filename: str
