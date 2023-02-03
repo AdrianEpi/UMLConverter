22c5fa02110e292f64e1e34df10fd27324e9b506
Adrian Epifanio Rodriguez Hernandez <adrianepi@gmail.com>
2023-02-03 22:09:51 +0000

    Added metricClass.py

---
+++ app/modules/metric_module/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#   @Proyect:            UMLConverter
+#   @Author:             Adrian Epifanio
+#   @File:               __init__.py
+#   @Author:             Adrian Epifanio
+#   @Date:               2023-02-02 13:40:00
+#   @Email:              adrianepi@gmail.com
+#   @GitHub:             https://github.com/AdrianEpi
+#   @Last Modified by:   Adrian Epifanio
+#   @Last Modified time: 2023-02-02 13:40:00
+#   @Description:        ...
---
+++ app/modules/metric_module/metricClass.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+#   @Proyect:            UMLConverter
+#   @Author:             Adrian Epifanio
+#   @File:               metricClass.py
+#   @Author:             Adrian Epifanio
+#   @Date:               2023-02-02 13:43:16
+#   @Email:              adrianepi@gmail.com
+#   @GitHub:             https://github.com/AdrianEpi
+#   @Last Modified by:   Adrian Epifanio
+#   @Last Modified time: 2023-02-02 14:10:45
+#   @Description:        ...
+
+
+class MetricClass:
+
+	classID: int 				# Class identifier
+	children: list 				# List of children classIDs inherited from this class
+	noc: list 					# List of children  grandchildren... classIDs
+	inheritance: list 			# List o classes classIDs of which this class is inherited of
+	include: list 				# List of classIDs from another class where this class is included
+	codeLines: int 				# Ammount of code lines (no empty ones or {})
+	commentLines: int 			# Ammount of commented lines
+	packageID: int 				# Package name
+	cc: int 					# Class coupling (ACO)
+	lcom: float					# Lack of cohesion in methods
+	ccd: float 					# Code comments density
+
+	def __init__(self, id: str):
+		self.classID = id 
+		self.children = [] 
+		self.noc = [] 
+		self.inheritance = [] 
+		self.include = [] 
+		self.codeLines = 0 
+		self.commentLines = 0 
+		self.packageID = 0 
+		self.cc = 0 
+		self.lcom = 0.0 
+		self.ccd = 0.0 
+
+	
+	def getClassID(self) -> int:
+		return 	self.classID
+
+	
+	def getChildren(self) -> list:
+		return 	self.children
+
+	
+	def getNoc(self) -> list:
+		return 	self.noc
+
+	
+	def getInheritance(self) -> list:
+		return 	self.inheritance
+
+	
+	def getInclude(self) -> list:
+		return 	self.include
+
+	
+	def getCodeLines(self) -> int:
+		return 	self.codeLines
+
+	
+	def getCommentLines(self) -> int:
+		return 	self.commentLines
+
+	
+	def getPackageID(self) -> int:
+		return 	self.packageID
+
+	
+	def getCc(self) -> int:
+		return 	self.cc
+
+	
+	def getLcom(self) -> float:
+		return 	self.lcom
+
+	
+	def getCcd(self) -> float:
+		return 	self.ccd
+
+	
+	def setClassID(self, newClassID:int):
+		self.classID = newClassID
+		
+	
+	def setChildren(self, newChildren:list):
+		self.children = newChildren
+		
+	
+	def setNoc(self, newNoc:list):
+		self.noc = newNoc
+		
+	
+	def setInheritance(self, newInheritance:list):
+		self.inheritance = newInheritance
+		
+	
+	def setInclude(self, newInclude:list):
+		self.include = newInclude
+		
+	
+	def setCodeLines(self, newCodeLines:int):
+		self.codeLines = newCodeLines
+		
+	
+	def setCommentLines(self, newCommentLines:int):
+		self.commentLines = newCommentLines
+		
+	
+	def setPackageID(self, newPackageID:int):
+		self.packageID = newPackageID
+		
+	
+	def setCc(self, newCc:int):
+		self.cc = newCc
+		
+	
+	def setLcom(self, newLcom:float):
+		self.lcom = newLcom
+		
+	
+	def setCcd(self, newCcd:float):
+		self.ccd = newCcd
+
+
+	def addChildren(self, id: int):
+		if id not in self.children:
+			self.children.append(i)
+
+
+	def addInheritance(self, id: int):
+		if id not in self.inheritance:
+			self.inheritance.append(i)
+
+
+	def addInclude(self, id: int):
+		if id not in self.include:
+			self.include.append(i)
+
+
+	def generateNOC(self, classList: list):
+		self.noc = self.children
+		for i in classList:
+			if self.getClassID in i.getInheritance():
+				self.noc.append(i.getClassID())
+
+
+	def calculateCCD(self):
+		self.ccd = self.commentLines / self.codeLines
